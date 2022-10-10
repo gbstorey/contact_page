7e8c18b71f069b4c68ae0552e75174b50760736b
gbstorey <gbstorey@asu.edu>
2022-10-09 23:42:42 -0700

    fixed awsmobile

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css"
 import App from './App';
 
 import {Amplify} from 'aws-amplify'
-import {awsmobile} from './aws-exports.js'
+import awsmobile from './aws-exports'
 Amplify.configure(awsmobile)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
