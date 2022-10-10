6a92fdc3153e401bfe4af28e43f78fdd3f826bb9
gbstorey <gbstorey@asu.edu>
2022-10-09 21:16:54 -0700

    simple react router setup: home, about, users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-function App() {
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
+function Home() {
+  return <>
+  <h2>Home</h2>
+  <nav>
+        <Link to="/about">About</Link>
+  </nav>
+  <nav>
+        <Link to="/users">Users</Link>
+  </nav>
+  </>;
+}
+
+function About() {
+  return <>
+  <h2>About</h2>
+    <nav>
+        <Link to="/">Home</Link>
+    </nav>
+  </>;
+}
+
+function Users() {
+  return <>
+  <h2>Users</h2>
+    <nav>
+        <Link to="/">Home</Link>
+    </nav>
+  </>;
+}
+
+export default function App() {
   return (
-    <h1>hi</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="users" element={<Users />} />
+    </Routes>
   );
 }
 
-export default App;
