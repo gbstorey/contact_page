105cb7309eaf409d561caece1302edee28e1bb2b
gbstorey <gbstorey@asu.edu>
2022-10-09 23:18:45 -0700

    implement oauth modality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import SignUpComponent from "./components/signup/SignUpComponent";
+import {Auth} from 'aws-amplify'
 
 function App() {
   return (
     <>
         <SignUpComponent />
+          <div className="App">
+          <header className="App-header">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+          </header>
+        </div>
     </>
   );
 }
