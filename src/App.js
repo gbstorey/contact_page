b09b33d3830ad5121ddeb572323d0ff3b6b68975
gbstorey <gbstorey@asu.edu>
2022-10-11 15:53:28 -0700

    removed breaking aws imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import SignUpComponent from "./components/signup/SignUpComponent";
-import {Auth} from 'aws-amplify'
 
 function App() {
   return (
     <>
         <SignUpComponent />
-          <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
-          </header>
-        </div>
     </>
   );
 }
